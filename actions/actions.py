6e3fa28c5dec5686b1c08dcf4c069b64cfec7a28
Althaf Azad <althafazad@Althafs-MacBook-Air.local>
2024-08-31 17:12:37 +0530

    Updated the doc

--- actions/actions.py
+++ actions/actions.py
@@ -1,109 +1,54 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-# actions.py
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# import pandas as pd
-
-# # Load the dataset
-# df = pd.read_csv('/Users/althafazad/Documents/Personal Projects/Recommendatoin_ChatBot/actions/universities.csv')
-
-# class ActionUniversityRank(Action):
-#     def name(self) -> Text:
-#         return "action_university_rank"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         university = tracker.get_slot('university')
-#         matched_rows = df[df['University_name'].str.contains(university, case=False, na=False)]
-#         if not matched_rows.empty:
-#             rank = matched_rows.iloc[0]['Rank']
-#             response = f"{university} is ranked {rank}."
-#         else:
-#             response = "I couldn't find the ranking information for that university."
-
-#         dispatcher.utter_message(text=response)
-#         return []
-
-# class ActionUniversityLocation(Action):
-#     def name(self) -> Text:
-#         return "action_university_location"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         university = tracker.get_slot('university')
-#         matched_rows = df[df['University_name'].str.contains(university, case=False, na=False)]
-#         if not matched_rows.empty:
-#             location = matched_rows.iloc[0]['Location']
-#             response = f"{university} is located in {location}."
-#         else:
-#             response = "I couldn't find the location for that university."
-
-#         dispatcher.utter_message(text=response)
-#         return []
-
-# class ActionUniversityFee(Action):
-#     def name(self) -> Text:
-#         return "action_university_fee"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         university = tracker.get_slot('university')
-#         matched_rows = df[df['University_name'].str.contains(university, case=False, na=False)]
-#         if not matched_rows.empty:
-#             fee = matched_rows.iloc[0]['Fee']
-#             response = f"The tuition fee for {university} is {fee}."
-#         else:
-#             response = "I couldn't find the fee information for that university."
-
-#         dispatcher.utter_message(text=response)
-#         return []
-
-
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import pandas as pd
+from rasa_sdk.events import SlotSet
 
 df = pd.read_csv('/Users/althafazad/Documents/Personal Projects/Recommendatoin_ChatBot/actions/universities.csv')
 
 class ActionUniversityRank(Action):
     def name(self) -> Text:
-        return "action_university_rank"
+        return "action_find_university_rank"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         university = tracker.get_slot('university')
-        # university = "Lancaster University"
-        print("university",university)
         matched_rows = df[df['University_name'].str.contains(university, case=False, na=False)]
         if not matched_rows.empty:
             rank = matched_rows.iloc[0]['UK_rank']
-            response = f"{university} is ranked {rank}."
+            response = f"{university} univeristy rank is {rank},"
         else:
             response = "I couldn't find the ranking information for that university."
+
+        dispatcher.utter_message(text=response)
+        return [SlotSet("university_rank_found", True)]
+
+
+class ActionFindUniversitiesByRegion(Action):
+
+    def name(self) -> Text:
+        return "action_find_universities_by_region"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Extract region entity from the user's message
+        region = tracker.get_slot("region")
+        
+        # Check if the region is not None
+        if region:
+            # Filter universities by region
+            filtered_df = df[df['region'].str.lower() == region.lower()]
+
+            # Get the list of universities in the specified region
+            universities = filtered_df['name'].tolist()
+
+            if universities:
+                response = f"The universities in {region} are: {', '.join(universities)}."
+            else:
+                response = f"Sorry, I couldn't find any universities in {region}."
+        else:
+            response = "Please specify a region to search for universities."
 
         dispatcher.utter_message(text=response)
         return []

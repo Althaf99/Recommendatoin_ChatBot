28b3d74b553290a014ebdf180912810c9154b05f
Althaf Azad <althafazad@Althafs-MacBook-Air.local>
2024-09-18 22:42:56 +0530

    Updated the registraion

--- actions/controller.py
+++ actions/controller.py
@@ -1,5 +1,5 @@
-import asyncio
-from flask import Flask,request, jsonify
+import base64
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
 from rasa.core.agent import Agent
 from rasa.core.utils import EndpointConfig
@@ -10,15 +10,16 @@ CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
-agent = Agent.load("/Users/althafazad/Documents/Personal Projects/Recommendatoin_ChatBot/models/20240916-155306-sienna-pot.tar.gz", action_endpoint=action_endpoint)
+agent = Agent.load("/Users/althafazad/Documents/Personal Projects/Recommendatoin_ChatBot/models/20240918-213316-wintry-style.tar.gz", action_endpoint=action_endpoint)
 
 @app.route('/send_message', methods=['POST'])
-async def send_message():
+def send_message():
     data = request.json
     session_id = data['session_id']
     text = data['text']
 
-    responses = await agent.handle_text(text, sender_id=session_id)
+    # Handle text using Rasa agent
+    responses = agent.handle_text(text, sender_id=session_id)
     
     if responses and 'intent' in responses[0]:
         bot_responses = [{'text': response['text']} for response in responses]
@@ -26,7 +27,6 @@ async def send_message():
         bot_responses = [{'text': 'I did not understand that. Could you please rephrase?'}]
 
     return jsonify(bot_responses)
-
 
 if __name__ == '__main__':
     socketio.run(app, port=5005)
